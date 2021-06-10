e6a07d257954efac6cef47dec28263fbd37a7828
Noshhy <73740320+Noshhy@users.noreply.github.com>
2021-06-10 21:00:31 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,26 @@
+class Polygon {
+    constructor(x, y,r) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y,radius, options);
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      ellipseMode(RADIUS);
+      fill(255);
+      ellipse(0, 0, this.radius);
+      pop();
+    }
+  };
